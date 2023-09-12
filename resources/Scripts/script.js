c54536d476f117a28304c38eea7c4d7ea11dd80b
joshua nathaniel <ministerjoe456@gmail.com>
2023-09-12 12:17:25 +0100

    add intitial game setup.

--- resources/Scripts/script.js
+++ resources/Scripts/script.js
@@ -2,3 +2,8 @@ const Player = (name, symbol) => {
     return { name, symbol };
 }
 
+const Game = (() => {
+    const board = ["", "", "", "", "", "", "", "", ""];
+    let currentPlayer;
+    let gameOver = false;
+})
